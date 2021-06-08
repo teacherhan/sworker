b6083da749ba11a27686e2bc4859282b52291f95
Erhan <teacherhan@gmail.com>
2021-06-08 15:55:13 -0700

    working cache v01

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 // Mkae sure sw are supported
 
-if ("seviceWorker" in navigator) {
+if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("./sw.js")
+      .register("../sw_cached_pages.js")
       .then((reg) => console.log("Service Worker: Registered"))
       .catch((err) => console.log(`Service Worker: Error: ${err} `));
   });
