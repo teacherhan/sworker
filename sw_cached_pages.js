ed74605704046905742dcc77c20eef96c69746be
Erhan <erhanashraf@gmail.com>
2021-06-08 17:01:32 -0700

    Update sw_cached_pages.js

--- sw_cached_pages.js
+++ sw_cached_pages.js
@@ -36,7 +36,3 @@ self.addEventListener("activate", (e) => {
 
 // Call Fetch Event
 
-self.addEventListener("fetch", (e) => {
-  console.log("Service Worker: Fetching");
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-});
